76689af7ad8e187d59b9b5a97d75f6a2c81d2709
David Sypien <davidsypien@gmail.com>
2015-09-04 20:41:01 -0400

    bind form data to bill model

--- app/components/appController.js
+++ app/components/appController.js
@@ -3,6 +3,11 @@ var BillsApp = angular.module('BillsApp', []);
 
 angular.module('BillsApp')
 	.controller('AppCtrl', ['$scope', function($scope){
-	 	$scope.bill = {};
-	 	$scope.sigPad = {width: 400, height: 200};
+	 	$scope.bill = {
+
+	 	};
+
+	 	$scope.save = function(){
+	 		console.log($scope.bill.sigPad.toDataURL());
+	 	};
 	}]);
