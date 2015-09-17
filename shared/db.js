317272878cc34e553f3e0fd5bd4655fe1a25dd12
David Sypien <davidsypien@gmail.com>
2015-09-16 23:09:35 -0400

    smh'

--- shared/db.js
+++ shared/db.js
@@ -1,8 +1,6 @@
 var orm = require("orm");
 
 module.exports.Bill = null;
-module.exports.Snapshot = null;
-module.exports.Signature = null;
 
 module.exports.init = function(callback){
 	try{
@@ -18,45 +16,16 @@ module.exports.init = function(callback){
 				name: String,
 				service_description: String,
 				price: Number,
-				description: String
+				snapshots: []
 			});
 
-			var Snapshot = db.define("Snapshot", {
-				photo : Buffer
-			});
-
-			var Signature = db.define("Signature", {
-				photo : Buffer
-			});
-
-			//Associations
-
-			// Bill.hasMany("snapshots",
-			// 	{photo : Buffer},
-			// 	{key : true, autoFetch : true}
-			// );
-
-			// Bill.hasMany("signa")
-
-			Snapshot.hasOne("Bill", Bill, {
-			    reverse : "snapshots"
-			});
-
-			Signature.hasOne("Bill", Bill, {
-			    reverse : "signatures"
-			});
-
-			Bill.sync(function(err){});
-			Snapshot.sync(function(err){});
-			Signature.sync(function(err){});
+			Bill.sync(function(err){});	
 
 			if(err){
 				callback(err);
 			}
 			else{
 				module.exports.Bill = Bill;
-				module.exports.Snapshot = Snapshot;
-				module.exports.Signature = Signature;
 
 				callback(null);
 			}
