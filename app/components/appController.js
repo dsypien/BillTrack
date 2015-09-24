0f90653f18cb1c38f12be84ba1a4b62a1f0bb051
David Sypien <davidsypien@gmail.com>
2015-09-23 23:17:39 -0400

    add routing

--- app/components/appController.js
+++ app/components/appController.js
@@ -1,5 +1,17 @@
 
-var BillsApp = angular.module('BillsApp', []);
+var BillsApp = angular.module('BillsApp', ['ngRoute']);
+
+BillsApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/bill', {
+        templateUrl: 'partials/bill.html',
+        controller: 'AppCtrl'
+      }).
+      otherwise({
+        redirectTo: '/bill'
+      });
+  }]);
 
 angular.module('BillsApp')
 	.controller('AppCtrl', ['$scope', function($scope){
